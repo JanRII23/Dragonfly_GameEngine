34b2755949ff0996c8bb6c336b895af70c963875
JRII_Ataka <jrejasii@gmail.com>
2023-01-04 17:21:46 -0600

    starting dragonfly game engine guide

---
+++ AtakaEngine/AtakaEngine/Main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+//This has the SFML and the dragonfly lib imported (but not sure it if its properly done though
+
+//int main() {
+//	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML Works");
+//
+//	while (window.isOpen())
+//	{
+//		window.clear(sf::Color::Black);
+//		sf::RectangleShape rs(sf::Vector2f(100, 100));
+//		window.draw(rs);
+//		window.display();
+//	}
+//}
